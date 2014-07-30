64345770e6b74be4a78722203662f47ac890b07f
Vipul Garg <vipul261@gmail.com>
2014-07-31 00:12:57 +0530

    Added mongo connection

--- web.js
+++ web.js
@@ -2,6 +2,19 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
