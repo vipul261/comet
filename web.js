1a4a2aca05b6447a938071525e041b612f1943ab
Vipul Garg <vipul261@gmail.com>
2014-08-01 00:53:50 +0530

    added sample data in mongo

--- web.js
+++ web.js
@@ -4,22 +4,38 @@ var app = express();
 
 var mongo = require('mongodb');
 
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
+var mongoUri = process.env.MONGOLAB_URI ||  process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
+var collections = ["artworks", "projects"];
+
+mongo.Db.connect(mongoUri, collections);
+
+mongo.Db.artworks.save({
+	id: 1,
+	name: 'Emotion',
+	price: 100,
+	likes: 10
+}, function( err, saved){
+	if( err || !saved ){
+		console.log('User not saved');
+	}
+	else{
+		console.log('User saved');
+	}
 });
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Comet says...Hello World!');
+  mongo.Db.find({id: 1}, function(err, artwork){
+  	if( err || !artwork){
+  		 response = "No Artwork Found found";	
+  	}
+  	else return artwork.name;
+  });
+  res.send(response);
 });
+
 app.get('/sayHi', function(req, res) {
   res.send('Comet says...HIIIIIIIIIII!!!');
 });
