9c0e3b9725d9ab0201b3acea389b283d7280ce4d
vipul261 <vipul261@gmail.com>
2015-08-23 16:33:06 +0530

    removed mongo references

--- web.js
+++ web.js
@@ -2,39 +2,39 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
 
-var mongoUri = process.env.MONGOLAB_URI ||  process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+//var mongoUri = process.env.MONGOLAB_URI ||  process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 var collections = ["artworks", "projects"];
 
-mongo.Db.connect(mongoUri, collections);
+//mongo.Db.connect(mongoUri, collections);
 
-mongo.Db.artworks.save({
-	id: 1,
-	name: 'Emotion',
-	price: 100,
-	likes: 10
-}, function( err, saved){
-	if( err || !saved ){
-		console.log('User not saved');
-	}
-	else{
-		console.log('User saved');
-	}
-});
+//mongo.Db.artworks.save({
+//	id: 1,
+//	name: 'Emotion',
+//	price: 100,
+//	likes: 10
+//}, function( err, saved){
+//	if( err || !saved ){
+//		console.log('User not saved');
+//	}
+//	else{
+//		console.log('User saved');
+//	}
+//});
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  mongo.Db.find({id: 1}, function(err, artwork){
-  	if( err || !artwork){
-  		 response = "No Artwork Found found";	
-  	}
-  	else return artwork.name;
-  });
-  res.send(response);
-});
+//app.get('/', function(req, res) {
+//  mongo.Db.find({id: 1}, function(err, artwork){
+//  	if( err || !artwork){
+//  		 response = "No Artwork Found found";	
+//  	}
+//  	else return artwork.name;
+//  });
+//  res.send(response);
+//});
 
 app.get('/sayHi', function(req, res) {
   res.send('Comet says...HIIIIIIIIIII!');
